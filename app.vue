680908f26c6ec02d1c1eccc163d78f9c160fe0b4
Timi <oblacishere@gmail.com>
2023-12-08 08:45:13 +0100

    add loginForm component

---
+++ app.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+
+    <LoginForm />
+  </div>
+</template>
+<script>
+// import LoginForm from './components/LoginForm.vue';
+</script>
+<style>
+form {
+  max-width: 300px;
+  border: 1px solid #eee;
+  padding: 20px;
+  border-radius: 10px;
+}
+label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+input {
+  width: 100%;
+  height: 40px;
+  border: 1px solid #eee;
+  border-radius: 8px;
+  padding-left: 20px;
+  box-sizing: border-box;
+  margin-bottom: 20px;
+}
+
+button {
+  width: 100%;
+  border-radius: 10px;
+  border: 0;
+  height: 40px;
+  background-color: #e0e0e0;
+}
+</style>
